4bcf3572691a04e1cf7813c71a27a236585e4ebb
Rory Walker <redrory@gmail.com>
2012-01-06 07:27:53 -0500

    Grabbing public data

--- app/controllers/show_controller.rb
+++ app/controllers/show_controller.rb
@@ -1,7 +1,7 @@
 class ShowController < ApplicationController
 
 	def show
-		@graph = Koala::Facebook::API.new(AAAAAAITEghMBAIZCwxH2O2QZCBdaqYsGsNdoG8cIE4qZCgFoRIOIMQOZBnifo7SfQXjUsPdZCZCdH1uZCBvhnVvQNar0gVZBbnnxITdqEW6WoQZDZD)
-		@rory = @graph.get_connections("redrorywalker", "likes")
+		@graph = Koala::Facebook::API.new
+		@rory = @graph.get_connections("redrorywalker")
 	end
 end
